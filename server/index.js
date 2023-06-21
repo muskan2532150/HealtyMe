369652ace11a11ecc52f815dcad104a148d450a9
muskan2532150 <rg2532150@gmail.com>
2023-06-21 13:08:16 +0530

    Build server and mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
 const port = 3001;
 
+const uri =process.env.ATLAS_URI;
 
-app.get('/',(req,res)=>{
-    res.send('Hiiii');
-})
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(uri);
+console.log('Connection build||||') 
+}
 
 app.listen(port,()=>{
     console.log("Connection build");
