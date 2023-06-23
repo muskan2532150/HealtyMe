26be190554ad4308b9cfbcb3ca408a8cfb37d540
muskan2532150 <rg2532150@gmail.com>
2023-06-23 12:40:35 +0530

    Add routes for models

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  mount Rswag::Api::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :user_profiles
+        resources :orders
+      end
+      resources :products
+    end
+  end
 end
