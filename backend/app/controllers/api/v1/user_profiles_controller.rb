e19fea6f9b5a896a291e743baccc759e40c54550
muskan2532150 <rg2532150@gmail.com>
2023-06-23 12:40:35 +0530

    Add method for userprofile controller

--- backend/app/controllers/api/v1/user_profiles_controller.rb
+++ backend/app/controllers/api/v1/user_profiles_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::UserProfilesController < ApplicationController
+    before_action User_profile_params
+
+    def show
+        render @profile
+    end
+
+    private
+
+    def user_profile_find
+        @user = User.find(params[:user_id])
+        @profile = @user.user_profile
+    end
+
+    def User_profile_params
+        require.(:user_profile).permit(:user_id,:phone_no,:profile_pic,:address)
+    end
 end
