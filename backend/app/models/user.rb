751179cfae57051a4c2f4a89790ff0eca737aa47
muskan2532150 <rg2532150@gmail.com>
2023-06-23 12:40:35 +0530

    Update user model for profile nested attributes

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
     validates :password, presence: true
 
     has_one :user_profile, dependent: :destroy
+    accepts_nested_attributes_for :user_profile
     has_many :orders, dependent: :destroy
 end
