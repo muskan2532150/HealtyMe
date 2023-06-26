9c626f08f87320c453a92bcf72a1502c05afa06b
muskan2532150 <rg2532150@gmail.com>
2023-06-26 12:40:35 +0530

    Add user example in seed file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# @first_user = User.new(name: 'muskan', email: 'test@gmail.com', password: 'password')
+# @first_userprofile = UserProfile.new(address: 'address 1', profile_pic: 'profile_pic 1', phone_no: 123456789)
+# {
+#     "user":{
+#       "name":"Muskan",
+#       "email":"test@email.com",
+#       "password":"123",
+#       "user_profile_attributes":{
+#         "address":"testaddresss",
+#         "profile_pic":"test.png",
+#         "phone_no":123490
+#       }
+#       }
+#   }
