f731a0ebde61b88744cdb6cd61eb209f9ad81965
muskan2532150 <rg2532150@gmail.com>
2023-06-26 12:40:35 +0530

    Add Footer component

---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+          <footer className="footer">
+            <div className="container">
+              <p>{new Date().getFullYear()} HealthyMe</p>
+            </div>
+          </footer>
+    </div>
+  )
+}
+
+export default Footer
