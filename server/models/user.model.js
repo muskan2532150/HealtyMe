727c25194f7a5cab5ba263f348b6091a38f65e1d
muskan2532150 <rg2532150@gmail.com>
2023-06-27 23:45:50 +0530

    Update User Schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,8 +4,10 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     username: {type: String, required: true,trim: true },
-    profile_pic: {type: String,required: true},
-    
+    profile_pic: {type: String},
+    email: {type:String,required: true},
+    password: {type:String,required: true}
+
 }, {
 timestamps:true
 });
