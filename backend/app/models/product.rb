bd41f8e5ed12152b402b8c0600fddb1e4582d8db
muskan2532150 <rg2532150@gmail.com>
2023-06-30 12:40:35 +0530

    Update Product model

--- backend/app/models/product.rb
+++ backend/app/models/product.rb
@@ -2,5 +2,9 @@ class Product < ApplicationRecord
     validates :name, presence: true
     validates :img, presence: true
     validates :price, presence: true
-
+    validates :category, presence: true
+    validates :company, presence: true
+    validates :description, presence: true
+    validates :rating, presence: true
+    validates :totalCount, presence: true
 end
