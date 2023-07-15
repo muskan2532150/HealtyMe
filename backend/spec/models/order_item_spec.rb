6933115b2caa6dbbe7b7e81223ec3ca65ac733e9
muskan2532150 <rg2532150@gmail.com>
2023-07-15 12:40:35 +0530

    Remove SideFilter component file

--- backend/spec/models/order_item_spec.rb
+++ backend/spec/models/order_item_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
   before(:each) do
-    @first_orderitem = OrderItem.new(product: 'Product1', quantity: 3, price: 32)
+    @first_orderitem = OrderItem.new(product_name: 'sdd', quantity: 3, price: 32)
   end
 
-  context 'OrderItem model validations' do
+  context 'Model validations' do
     it 'is valid with valid attributes' do
       expect(@first_orderitem).to be_valid
     end
 
     it 'is not valid without a product' do
-      @first_orderitem.product = nil
+      @first_orderitem.product_name = nil
       expect(@first_orderitem).to_not be_valid
     end
 
