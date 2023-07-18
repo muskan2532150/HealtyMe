9a2b36e635cd4ab6b18339d39c3ebc6ab1fff4a7
muskan2532150 <rg2532150@gmail.com>
2023-07-18 12:40:35 +0530

    Update Main css with Tailwind import and extra css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import store from './redux/store'
 import { Provider } from 'react-redux'
-import './main.css'
+import './Main.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
