b9dfbc260b14264697f81f1e2aa59dd197086e75
muskan2532150 <rg2532150@gmail.com>
2023-07-19 12:40:35 +0530

    Add all import from component to import file

---
+++ client/src/Import.jsx
@@ -0,0 +1,6 @@
+export {default as Navbar} from "./Components/Navbar";
+export {default as Product} from "./Components/Product";
+export {default as LoginPage} from "./Components/LoginPage";
+export {default as Cart} from "./Components/Cart";
+export {default as Card} from "./Components/Card";
+export {default as UserDetail} from "./Components/UserDetail";
