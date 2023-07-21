3d04da74848c4e7a26ad2426a5e89ea8bd2a2031
muskan2532150 <rg2532150@gmail.com>
2023-07-21 12:40:35 +0530

    Upgrade CardGrid with Tailwind component

--- client/src/Components/CardGrid.jsx
+++ client/src/Components/CardGrid.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from './Card';
+import { Link } from 'react-router-dom';
 
 const CardGrid = ({ products, cardsPerPage, currentPage }) => {
   const indexOfLastCard = currentPage * cardsPerPage;
@@ -7,10 +7,34 @@ const CardGrid = ({ products, cardsPerPage, currentPage }) => {
   const currentCards = products.slice(indexOfFirstCard, indexOfLastCard);
 
   return (
-    <div className="product-grid">
-      {currentCards.map((product) => (
-        <Card key={product.id} data={product} />
-      ))}
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900 text-center">Products</h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {currentCards.map((product) => (
+            <Link to={{ pathname: `/product/${product.id}`, state: product }}
+            >  <div key={product.id} className="group relative">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                  <img
+                    src={product.attributes.img}
+                    alt={product.attributes.name}
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                </div>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-700">
+                      {product.attributes.name}
+                    </h3>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">{product.attributes.price}</p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
