b1d97c4f1e0894005e989d74df0d5f9a3f73e47d
muskan2532150 <rg2532150@gmail.com>
2023-07-26 12:40:35 +0530

    upgrade Cart with tailwind component

--- client/src/Components/Cart.jsx
+++ client/src/Components/Cart.jsx
@@ -2,13 +2,15 @@ import { Fragment, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate,Link } from 'react-router-dom'; 
 
 
 const Cart = () => {
   const  cart  = useSelector((state) => state.cart);
   const [open, setOpen] = useState(true)
   const navigate = useNavigate();
+  let totalamount = 0;
+
 
   const handleClose = () => {
     setOpen(false);
@@ -62,12 +64,12 @@ const Cart = () => {
                         <div className="mt-8">
                           <div className="flow-root">
                             <ul role="list" className="-my-6 divide-y divide-gray-200">
-                              {cart.map((product) => (
+                              {cart.length === 0 ? <p className='text-center '> Continue Shopping</p> : cart.map((product) => (
                                 <li key={product.id} className="flex py-6">
                                   <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
                                     <img
-                                      src={product.image}
-                                      alt={product.name}
+                                      src={product.attributes.img}
+                                      alt={product.attributes.name}
                                       className="h-full w-full object-cover object-center"
                                     />
                                   </div>
@@ -76,13 +78,14 @@ const Cart = () => {
                                     <div>
                                       <div className="flex justify-between text-base font-medium text-gray-900">
                                         <h3>
-                                          <a href={product.href}>{product.name}</a>
+                                        <Link to={`/product/${product.id}`}>
+                                          {product.attributes.name}</Link>
                                         </h3>
-                                        <p className="ml-4">{product.price}</p>
+                                        <p className="ml-4">{product.attributes.price}</p>
+                                        <p className='hidden'>{totalamount+= product.attributes.price}</p>
                                       </div>
                                     </div>
                                     <div className="flex flex-1 items-end justify-between text-sm">
-                                      <p className="text-gray-500">Qty {product.quantity}</p>
   
                                       <div className="flex">
                                         <button
@@ -104,7 +107,7 @@ const Cart = () => {
                       <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
                         <div className="flex justify-between text-base font-medium text-gray-900">
                           <p>Subtotal</p>
-                          <p>$260.00</p>
+                          <p>{totalamount}</p>
                         </div>
                         <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
                         <div className="mt-6">
